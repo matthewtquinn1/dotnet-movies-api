d2c8bde8433f04214e8e7ff59cca9213ab956ce1
Matthew Quinn <full.staq.ltd@gmail.com>
2025-01-30 22:08:12 +0100

    Add endpoint for getting movie by ID

--- MovieReviews.Api/Controllers/MoviesController.cs
+++ MovieReviews.Api/Controllers/MoviesController.cs
@@ -37,6 +37,23 @@ namespace MovieReviews.Api.Controllers
             }
         }
 
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            // TODO: Replace try catch with a middleware that catches exceptions and returns a proper response.
+            try
+            {
+                var movie = await _mediator.Send(new GetMovieByIdQuery(id));
+
+                return movie is null ? NotFound() : Ok(movie);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to find movie");
+                throw;
+            }
+        }
+
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] CreateMovieCommand command)
         {
---
+++ MovieReviews.Application/Movies/Queries/GetMovieByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using MovieReviews.Application.Mappings;
+using MovieReviews.Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieReviews.Application.Movies.Queries;
+
+public record GetMovieByIdQuery(Guid Id) : IRequest<MovieDto?>;
+
+public sealed class GetMovieByIdQueryHandler : IRequestHandler<GetMovieByIdQuery, MovieDto?>
+{
+    private readonly IApplicationContext _context;
+
+    public GetMovieByIdQueryHandler(IApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<MovieDto?> Handle(GetMovieByIdQuery request, CancellationToken cancellationToken)
+    {
+        var movie = await _context.Movies
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        return movie?.ToDto();
+    }
+}
